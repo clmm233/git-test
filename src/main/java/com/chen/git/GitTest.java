d8d81119f869996a26a0f3718f3b21fbd509340d
chen <2961460623@qq.com>
2022-08-15 11:11:48 +0800

    first commiat

---
+++ src/main/java/com/chen/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.chen.git;
+
+/**
+ * @author: 陈希来
+ * @date: 2022/8/15 11:07
+ * Created in changsha
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hallo git!");
+    }
+}
