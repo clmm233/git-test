4313c12c5e1bd7e89a4ab7a6dd1e4b60a0e19121
chen <2961460623@qq.com>
2022-08-15 11:26:05 +0800

    hot-fix commit1

--- src/main/java/com/chen/git/GitTest.java
+++ src/main/java/com/chen/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hallo git!");
         System.out.println("hallo git2");
+        System.out.println("hallo git3");
     }
 }
